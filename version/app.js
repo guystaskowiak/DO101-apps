8f47a724ab7649ffd07f3acfab655e3a36323243
Guy Staskowiak <guy.staskowiak@essers.com>
2020-05-08 09:40:51 +0200

    date

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is version 1 of the app.' + '\n';
+    response = 'Today is 8th May 2020' + '\n';
 
     //send the response to the client
     res.send(response);
